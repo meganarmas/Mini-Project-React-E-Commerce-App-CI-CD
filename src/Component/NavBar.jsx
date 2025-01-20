dbc12c006f0a75643f07b72a737db9d7ae0aa0b2
Megan <meganarmas@gmail.com>
2025-01-20 08:38:37 -0600

    Update NavBar.jsx

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,20 +1,51 @@
-import { Link } from "react-router-dom";
-import { Navbar, Nav} from "react-bootstrap";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function NavigationBar() {
-    return(
-        <Navbar className="navbar" expand="lg">
-            <Navbar.Brand as={Link} to="/home">Home</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/login">Login</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/logout">Logout</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/shopping-cart">Shopping Cart</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/products">Products</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/catalog">Product Catalog</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/order-history">Order History</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/create">Register</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/update">Update Account</Navbar.Brand> <br />
-            <Navbar.Brand as={Link} to="/delete">Delete Account</Navbar.Brand> <br />
+    return (
+        <Navbar expand="lg" className="p-2" style={{ width: "100%", backgroundColor: "beige" }}>
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/" className="fw-bold text-uppercase">
+                   E-Commerce App
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbar-nav" />
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/" className="nav-item-custom">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/login" className="nav-item-custom">
+                            Login
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/logout" className="nav-item-custom">
+                            Logout
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/shopping-cart" className="nav-item-custom">
+                            Shopping Cart
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/products" className="nav-item-custom">
+                            Products
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/catalog" className="nav-item-custom">
+                            Product Catalog
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/order-history" className="nav-item-custom">
+                            Order History
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/create" className="nav-item-custom">
+                            Register
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/update" className="nav-item-custom">
+                            Update Account
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/delete" className="nav-item-custom">
+                            Delete Account
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 }
